8e04d9e7c67604b9da889b51aa4143d5346699a1
crosstyan <crosstyan@outlook.com>
2023-09-15 15:22:01 +0000

    README

---
+++ README.md
@@ -0,0 +1,26 @@
+# C++ 20 Coroutine
+
+- [C++ coroutines: Getting started with awaitable objects](https://devblogs.microsoft.com/oldnewthing/20191209-00/?p=103195)
+- [Coroutines: C++ vs Rust - Jonathan Müller - ACCU 2022](https://www.youtube.com/watch?v=aa43fYHgnfo)
+- [C++20 Coroutines - Complete Guide](https://www.youtube.com/watch?v=w-dmOHhBX9o)
+- [A map through the three major coroutine series - Old New Thing](https://devblogs.microsoft.com/oldnewthing/20210504-01/?p=105178)
+- [Deciphering C++ Coroutines - A Diagrammatic Coroutine Cheat Sheet - Andreas Weis - CppCon 2022](https://www.youtube.com/watch?v=J7fYddslH0Q)
+
+## Stackful
+
+- [Stackful Coroutines](https://www.boost.org/doc/libs/1_83_0/doc/html/boost_asio/overview/composition/spawn.html)
+- [C++ Coroutines Do Not Spark Joy - Hacker News](https://news.ycombinator.com/item?id=29064233)
+
+## Others
+
+- [folly/io/async](https://github.com/facebook/folly/blob/main/folly/io/async/README.md)
+
+## Python
+
+- [Trio – a friendly Python library for async concurrency and I/O](https://github.com/python-trio/trio)
+- [AnyIO](https://github.com/agronholm/anyio)
+
+## C++ coroutine library
+
+- [libcoro](https://github.com/jbaldwin/libcoro)
+- [X-Neon/kuro](https://github.com/X-Neon/kuro)
--- main.cpp
+++ main.cpp
@@ -3,46 +3,92 @@
 #include <variant>
 
 
+namespace co {
 /// https://en.cppreference.com/w/cpp/coroutine/coroutine_traits
 /* std::coroutine_traits<co_ret_t, ...> */
-struct co_ret_t {
-    struct co_promise_t {
-        co_ret_t get_return_object() {
-            return {std::coroutine_handle<promise_type>::from_promise(*this)};
+    struct ret_t {
+        struct answer_awaitable;
+
+        struct promise_t {
+            int val = 0;
+
+
+            // optional yield
+            answer_awaitable yield_value(int v) {
+                return {v};
+            }
+
+
+            /** some traits **/
+
+            ret_t get_return_object() {
+                return {std::coroutine_handle<promise_type>::from_promise(*this)};
+            }
+
+            // return an awaitable
+            // await_ready()
+            // await_suspend(std::coroutine_handle<promise_t> handle)
+            // await_resume()
+            answer_awaitable initial_suspend() {
+                std::cout << "initial_suspend; " << "val: " << val << std::endl;
+                return {val};
+            }
+
+
+            // start ↑ / shutdown ↓
+
+            void return_void() {}
+
+            void unhandled_exception() {}
+
+            // return an awaitable
+            answer_awaitable final_suspend() noexcept {
+                std::cout << "final_suspend; " << "val: " << val << std::endl;
+                return {val};
+            }
+
+        };
+
+        struct answer_awaitable {
+            int val;
+
+            answer_awaitable(int v) : val(v) {
+                std::cout << "answer_awaitable: " << val << std::endl;
+            }
+
+            // https://en.cppreference.com/w/cpp/coroutine/suspend_always
+            // true: suspend never
+            // false: suspend always
+            bool await_ready() noexcept { return false; }
+
+            void await_suspend(std::coroutine_handle<promise_t> handle) noexcept {
+                std::cout << "await_suspend" << std::endl;
+                handle.promise().val = this->val;
+            }
+
+            void await_resume() noexcept {}
+        };
+
+        /// trait
+        using promise_type = promise_t;
+
+        using handle_t = std::coroutine_handle<promise_type>;
+        handle_t handle;
+
+
+        ret_t(handle_t h) : handle(h) {};
+
+        int val() {
+            return handle.promise().val;
         }
-
-        // return an awaitable
-        // await_ready()
-        // await_suspend(std::coroutine_handle<co_promise_t> handle)
-        // await_resume()
-        std::suspend_always initial_suspend() { return {}; }
-
-        // start ↑ / shutdown ↓
-
-        void return_void() {}
-
-        void unhandled_exception() {}
-
-        // return an awaitable
-        std::suspend_always final_suspend() noexcept { return {}; }
     };
+}
 
-    /// trait
-    using promise_type = co_promise_t;
-
-    // my convention
-    using handle_t = std::coroutine_handle<promise_type>;
-    handle_t handle;
-    co_ret_t(handle_t h):handle(h){};
-
-    void resume(){
-        handle.resume();
-    }
-};
-
-co_ret_t hello_coroutine() {
+co::ret_t hello_coroutine() {
+    // this expression is still from caller?
     std::cout << "Hello coroutine!" << std::endl;
-    co_return;
+    co_await co::ret_t::answer_awaitable{42};
+    // co_yield 42;
 }
 
 // co_await
@@ -52,6 +98,7 @@ int main() {
     auto hello = hello_coroutine();
     std::cout << "suspend" << std::endl;
     std::cout << "resume" << std::endl;
-    hello.resume();
+    auto v = hello.val();
+    std::cout << "val: " << v << std::endl;
     return 0;
 }
