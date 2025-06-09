4f6fdceb67143617251fb49f0ca893aad4c93638
crosstyan <crosstyan@outlook.com>
2025-06-09 16:12:53 +0800

    refactor: Enhance coroutine support in main.cpp
    - Implemented a custom promise type for std::coroutine_traits to manage coroutine state.
    - Added methods for initial and final suspension, return value handling, and exception management.
    - Updated the main function to retrieve and print the result of the coroutine.

--- src/main.cpp
+++ src/main.cpp
@@ -5,28 +5,41 @@
 #include <variant>
 
 
+/** std::coroutine_traits<co_ret_t, ...>
+ * @see https://en.cppreference.com/w/cpp/coroutine/coroutine_traits
+ */
 template <typename R, typename... Args>
 struct std::coroutine_traits<std::future<R>, Args...> {
-	struct promise_type {};
+	struct promise_type {
+		std::promise<R> p;
+
+		/** like a constructor */
+		std::suspend_never initial_suspend() noexcept { return {}; }
+		/** like a destructor */
+		std::suspend_never final_suspend() noexcept { return {}; }
+		/** when the `co_return` is called */
+		void return_value(R value) {
+			p.set_value(value);
+		}
+		/** construct the return object (the first parameter of `coroutine_traits`) */
+		auto get_return_object() -> std::future<R> {
+			return p.get_future();
+		}
+		/** exception CANNOT leak from the coroutine */
+		void unhandled_exception() {
+			p.set_exception(std::current_exception());
+		}
+	};
 };
 
 namespace co {
-/// https://en.cppreference.com/w/cpp/coroutine/coroutine_traits
-/* std::coroutine_traits<co_ret_t, ...> */
-struct ret_t {
-};
-
-
 std::future<int> f() {
-	std::println("hello!");
 	co_return 42;
 }
 }
 
 
-// co_await
-// The unary operator co_await suspends a coroutine and returns control to the
-// caller.
 int main() {
-	co::f();
+	auto f = co::f();
+	std::println("{}", f.get());
 }
